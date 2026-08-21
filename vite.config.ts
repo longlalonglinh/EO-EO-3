d04360140f98450c274ff9a12e4634812a428f0b
longlalonglinh <nguyenhoanglinh1606@gmail.com>
2026-08-22 01:10:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+  base: '/EO-EO-3/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
